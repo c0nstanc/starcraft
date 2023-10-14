e58c4a3eb4069e45152f866e33989283564594f7
constantinos <constantinides_c@hotmail.com>
2023-10-14 17:16:14 +0300

    chore : upgrade angular libraries to their latest version

--- src/app/core/http/unit-statistics/service/http-unit-statistics.service.ts
+++ src/app/core/http/unit-statistics/service/http-unit-statistics.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequestOptions, HTTP_UNIT_STATISTICS } from '@core/http/base/model';
+import {
+  HttpRequestOptions,
+  HTTP_UNIT_STATISTICS,
+} from '@core/http/base/model';
 import { HttpBase } from '@core/http/base/service/http-base.abstract';
 import { environment } from '@env';
 import { Observable, of } from 'rxjs';
@@ -8,30 +11,24 @@ import { MOCK_UNIT_STATISTICS } from '../mock/data';
 import { HttpUnitStatistic } from '../model/unit-statistic.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpGarageProviderService extends HttpBase {
-
-  constructor(
-    private http: HttpClient,
-    // simplePageSpinnerService: SimplePageSpinnerService
-  ) {
-    super(
-      // simplePageSpinnerService
-    );
+  constructor(private http: HttpClient) {
+    super();
   }
 
-  getUnitStatistics(httpRequestOptions?: HttpRequestOptions): Observable<HttpUnitStatistic[]> {
-
-    const getUnitStatisticsRequest$ = this.http.get<HttpUnitStatistic[]>(HTTP_UNIT_STATISTICS());
+  getUnitStatistics(
+    httpRequestOptions?: HttpRequestOptions
+  ): Observable<HttpUnitStatistic[]> {
+    const getUnitStatisticsRequest$ = this.http.get<HttpUnitStatistic[]>(
+      HTTP_UNIT_STATISTICS()
+    );
 
     if (environment.mocking.unitStatistics) {
       return of(MOCK_UNIT_STATISTICS.data as HttpUnitStatistic[]);
-
     } else {
       return this.request(getUnitStatisticsRequest$, httpRequestOptions);
     }
-
-
   }
 }
